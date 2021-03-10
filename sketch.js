521e724eb026eabffd68402353caa4bfcdb6f9f3
darshil1 <74967354+darshil1@users.noreply.github.com>
2021-03-10 14:20:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,92 +1,70 @@
-var Example = Example || {};
+var wall, thickness;
+var bullet,speed, weight;
 
-Example.softBody = function() {
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Composites = Matter.Composites,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        World = Matter.World,
-        Bodies = Matter.Bodies;
+function setup() {
+  createCanvas(1600, 400);
 
-    // create engine
-    var engine = Engine.create(),
-        world = engine.world;
+  speed=random(223,321)
+  weight=random(30,52)
 
-    // create renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: 800,
-            height: 600,
-            showAngleIndicator: false
-        }
-    });
 
-    Render.run(render);
+  thickness=random(22,83)
 
-    // create runner
-    var runner = Runner.create();
-    Runner.run(runner, engine);
 
-    // add bodies
-    var particleOptions = { 
-        friction: 0.05,
-        frictionStatic: 0.1,
-        render: { visible: true } 
-    };
 
-    World.add(world, [
-        Composites.softBody(250, 100, 5, 5, 0, 0, true, 18, particleOptions),
-        Composites.softBody(400, 300, 8, 3, 0, 0, true, 15, particleOptions),
-        Composites.softBody(250, 400, 4, 4, 0, 0, true, 15, particleOptions),
-        // walls
-        Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-        Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-        Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-        Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
-    ]);
 
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.9,
-                render: {
-                    visible: false
-                }
-            }
-        });
+  
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
 
-    World.add(world, mouseConstraint);
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
+}
 
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
-    });
 
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        render: render,
-        canvas: render.canvas,
-        stop: function() {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
-    };
-};
+function draw() {
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-Example.softBody.for = '>=0.14.2';
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
-if (typeof module !== 'undefined') {
-    module.exports = Example.softBody;
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
+  }
+
+
+  drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
